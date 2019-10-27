8c76fe41cd712a0686a20c92fd46eebdd047ee8a
Luize <lr1965@nyu.edu>
2019-10-28 02:17:27 +0400

    Updated pictures

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$('.image').click(function() {
+  $('#cross').toggle('slow', function() {
+    // Animation complete.
+  });
+});
+
+
+
+$('.image2').click(function() {
+  $('#cross2').toggle('slow', function() {
+    // Animation complete.
+  });
+});
+
+
+$('.image3').click(function() {
+  $('#cross3').toggle('slow', function() {
+    // Animation complete.
+  });
+});
+
+
+$('.image4').click(function() {
+  $('#cross4').toggle('slow', function() {
+    // Animation complete.
+  });
+});
